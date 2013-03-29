ca118d7aa05554bf9de3312d0145a713fea0ce97
Timo Tijhof <krinklemail@gmail.com>
2013-03-29 01:37:33 +0100

    Add various files

---
+++ clerkbox-config.py
@@ -0,0 +1,19 @@
+# Configuration for clerkbot
+
+## Required
+nickname = "CVN-ClerkBot"
+password = "CVN-ClerkBot:591wxzR"
+realname = "Helper bot for the Countervandalism Network"
+HOST = "irc.freenode.net"
+PORT = 6667
+
+## Static
+channels = ["#countervandalism", "#cvn-staff", "#cvn-bots"]
+
+## MySQL config
+useMySQL = True
+sqlhost = "localhost"
+sqlport = 3306
+sqlname = "root"
+sqlpw = "root"
+schema = "cvnclerkbot"
