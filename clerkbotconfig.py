bc4237b7e2396e8980835c517a7df7e57dbfadf2
Timo Tijhof <krinklemail@gmail.com>
2016-02-18 19:45:43 +0000

    Update clerkbotconfig.py

---
+++ clerkbotconfig.py
@@ -0,0 +1,20 @@
+# Configuration for clerkbot
+
+# IRC server
+HOST = "irc.freenode.net"
+PORT = 6667
+
+# IRC user (used to identify with NickServ)
+nickname = "CVN-ClerkBot"
+password = "CVN-ClerkBot:591wxzR"
+
+# IRC initial channels to join onconnect
+channels = ["#countervandalism", "#cvn-staff", "#cvn-bots"]
+
+# MySQL config
+useMySQL = True
+sqlhost = "localhost"
+sqlport = 3306
+sqluser = "root"
+sqlpw = "root"
+sqldbname = "cvnclerkbot"
