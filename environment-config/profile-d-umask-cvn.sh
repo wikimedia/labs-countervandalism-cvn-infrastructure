546206987d99cdf5b599b0afe895e114d413797f
Timo Tijhof <krinklemail@gmail.com>
2023-01-15 22:07:49 +0000

    Update profile-d-umask-cvn.sh

--- environment-config/profile-d-umask-cvn.sh
+++ environment-config/profile-d-umask-cvn.sh
@@ -1,3 +1,7 @@
+# Inspired by WMF puppet
+# https://gerrit.wikimedia.org/g/operations/puppet/+/ba2083cc3c4b64c1b48f9e60317b40847ecb1aee/modules/deployment/files/umask-wikidev-profile-d.sh
+# -------
+
 # Set umask 002 for project-cvn
 # so that stuff isn't broken (e.g. interacting with git repos)
 if groups | grep -w -q project-cvn; then
